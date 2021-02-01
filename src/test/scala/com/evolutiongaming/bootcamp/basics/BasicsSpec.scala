81d6daba57cfab3ceb9475afc8847cac97e0cea5
Eriks Nukis <eriks.nukis@gmail.com>
2021-02-02 01:09:56 +0200

    One more test case added to "gcd"

--- src/test/scala/com/evolutiongaming/bootcamp/basics/BasicsSpec.scala
+++ src/test/scala/com/evolutiongaming/bootcamp/basics/BasicsSpec.scala
@@ -7,8 +7,8 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 class BasicsSpec extends AnyFlatSpec {
 
   "gcd" should "be calculated correctly" in {
-    val sampleLcdList = List((21, 7, 7), (9, 6, 3), (25,15,5))
-    for (sample <- sampleLcdList) {
+    val sampleGcdList = List((21, 7, 7), (9, 6, 3), (25,15,5), (5,14,1))
+    for (sample <- sampleGcdList) {
       gcd(sample._1, sample._2) shouldEqual sample._3
     }
   }
